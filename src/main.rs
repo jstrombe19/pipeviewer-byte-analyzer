5d42c2fb60a66a54ffd4eb7431539aa98c8998fa
jstrombe19 <jaredstromberg@gmail.com>
2021-09-22 20:47:41 -0600

    cleaned up dbg! uses, noted specific use cases and identified dbg! is not for logging.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
     let silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
-    dbg!(silent);
+    // dbg! macro example - NOTE: this is NOT for logging!
+    // dbg!(silent);
     let mut total_bytes = 0;
     loop {
         let mut buffer = [0; CHUNK_SIZE];
@@ -17,8 +18,10 @@ fn main() {
         // when using dbg!() if you are also using the += (assignment operator)
         // you will not get a value returned because the assignment operator does
         // not return a value - it returns the unit type or empty type ()
-        dbg!(total_bytes += num_read);
+        total_bytes += num_read;
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
-    eprintln!("num_read: {}", total_bytes);
+    if !silent {
+        eprintln!("{}", total_bytes);
+    }
 }
