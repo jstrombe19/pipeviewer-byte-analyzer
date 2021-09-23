365bfdd909a53069528a49de8579b666605d557f
jstrombe19 <jaredstromberg@gmail.com>
2021-09-22 20:42:15 -0600

    additional testing of environment variable, dbg! macro. updates incorporated in README.

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ an identical file was generated titled myfile2, which was checked for accuracy u
 ```bash
 diff myfile myfile2
 ```
+
+the additional implementation of the dbg! macro and an environment variable were tested using
+
+```bash
+echo "a string" | cargo run
+```
+
+however, because the environment variable was not previously declared, it returned **false**
+
+to test the assignment of the environment variable declared, the test was repeated with an explicit declaration
+
+```bash
+echo "a string" | PV_SILENT=something cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
+    let silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
+    dbg!(silent);
     let mut total_bytes = 0;
     loop {
         let mut buffer = [0; CHUNK_SIZE];
@@ -11,7 +14,10 @@ fn main() {
             Ok(x) => x,
             Err(_) => break,
         };
-        total_bytes += num_read;
+        // when using dbg!() if you are also using the += (assignment operator)
+        // you will not get a value returned because the assignment operator does
+        // not return a value - it returns the unit type or empty type ()
+        dbg!(total_bytes += num_read);
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
     eprintln!("num_read: {}", total_bytes);
