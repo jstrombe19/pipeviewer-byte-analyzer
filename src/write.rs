e4bc7f158f588764d09829fd8c77da31f8afada8
Jared Stromberg <jaredstromberg@gmail.com>
2021-09-24 13:07:38 -0600

    modified write.rs module to support its own infinite loop within the write function. NOTE: there is currently an empty vector declaration included as a placeholder for the vector which will be received from the stats mod which is necessary to compile.

--- src/write.rs
+++ src/write.rs
@@ -1,19 +1,31 @@
 use std::fs::File;
 use std::io::{self, BufWriter, ErrorKind, Result, Write};
+use std::sync::{Arc, Mutex};
 
-pub fn write(outfile: &str, buffer: &[u8]) -> Result<bool> {
+pub fn write(outfile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
   let mut writer: Box<dyn Write> = if !outfile.is_empty() {
     Box::new(BufWriter::new(File::create(outfile)?))
   } else {
     Box::new(BufWriter::new(io::stdout()))
   };
 
+  loop {  
+  // todo: receive vector from stats thread
+  let buffer: Vec<u8> = Vec::new(); // so we can compile
+  {
+    let quit = quit.lock().unwrap();
+    if *quit {
+      break;
+    }
+  }
   if let Err(e) = writer.write_all(&buffer) {
     if e.kind() == ErrorKind::BrokenPipe {
-      return Ok(false);
+      // stop the program cleanly
+      return Ok(());
     }
     return Err(e);
+    }
   }
 
-  Ok(true)
+  Ok(())
 }
