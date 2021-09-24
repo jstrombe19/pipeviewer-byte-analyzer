95e81bfbbc27b530b4fba3ba6b31d6916d0e9bb7
Jared Stromberg <jaredstromberg@gmail.com>
2021-09-24 12:34:05 -0600

    refactored all functionality into separate modules to support multithreading. all behavior re-tested and verified using sample files via command line.

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,37 @@
-use clap::{App, Arg};
-use std::fs::File;
-use std::env;
-use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
-
-const CHUNK_SIZE: usize = 16 * 1024;
+use pipeviewer_byte_analyzer::{args::Args, read, stats, write};
+use std::io::Result;
 
 fn main() -> Result<()> {
-  let matches = App::new("pipeviewer")
-    .arg(Arg::with_name("infile")
-      .help("Read from a file instead of stdin")
-    )
-    .arg(Arg::with_name("outfile")
-      .short("o")
-      .long("outfile")
-      .takes_value(true)
-      .help("Write output to a file instead of stdout"),
-    )
-    .arg(Arg::with_name("silent")
-      .short("s")
-      .long("silent")
-    )
-    .get_matches();
-    let infile = matches.value_of("infile").unwrap_or_default();
-    let outfile = matches.value_of("outfile").unwrap_or_default();
-    let silent = if matches.is_present("silent") {
-      true
-    } else {
-      !env::var("PV_SILENT").unwrap_or_default().is_empty()
-    };
-    // to view the outcome or values of the variables, use the dbg! macro below
-    // dbg!(infile, outfile, silent);
-    // dbg! macro example - NOTE: this is NOT for logging!
-    // dbg!(silent);
+  
+  // to view the outcome or values of the variables, use the dbg! macro below
+  // dbg!(infile, outfile, silent);
+  // dbg! macro example - NOTE: this is NOT for logging!
+  // dbg!(silent);
 
-    let mut reader: Box<dyn Read> = if !infile.is_empty() {
-      Box::new(BufReader::new(File::open(infile)?))
-    } else {
-      Box::new(BufReader::new(io::stdin()))
-    };
+  let args = Args::parse();
+  let mut total_bytes = 0;
+  loop {
+      // original position of buffer
+      // let mut buffer = [0; CHUNK_SIZE];
 
-    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-      Box::new(BufWriter::new(File::create(outfile)?))
-    } else {
-      Box::new(BufWriter::new(io::stdout()))
-    };
+      // when using dbg!() if you are also using the += (assignment operator)
+      // you will not get a value returned because the assignment operator does
+      // not return a value - it returns the unit type or empty type ()
 
-    let mut buffer = [0; CHUNK_SIZE];
-    let mut total_bytes = 0;
-    loop {
-        // original position of buffer
-        // let mut buffer = [0; CHUNK_SIZE];
-        let num_read = match reader.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(x) => x,
-            Err(_) => break,
-        };
-        // when using dbg!() if you are also using the += (assignment operator)
-        // you will not get a value returned because the assignment operator does
-        // not return a value - it returns the unit type or empty type ()
-        total_bytes += num_read;
-        // simplest solution to check if the value assignment returns an error:
-        // io::stdout().write_all(&buffer[..num_read])?;
-        // this syntax is intentionally not used in this case to allow the custom
-        // case to handle a broken pipe error type
-        if !silent {
-            eprint!("\r{}", total_bytes);
-        }
-        if let Err(e) = writer.write_all(&buffer[..num_read]) {
-            // this is the destructured way to access ErrorKind, and requires the explicit
-            // inclusion of ErrorKind in the std::io use statement above
-            // the other way to do this is to drill down each level:
-            // if e.kind() == std::io::ErrorKind::BrokenPipe {}
-            if e.kind() == ErrorKind::BrokenPipe {
-                break;
-            }
-            // standard error return
-            // return Err(e);
-            // can also be customized to handle errors differently
-            eprintln!(
-                "I'm sorry, Dave. I'm afraid I can't handle this: {}",
-                e.to_string()
-            );
-            // in addition to customized error messages, you can also
-            // assign custom standard error codes
-            std::process::exit(1);
-        }
-    }
-    if !silent {
-        eprintln!("\r{}", total_bytes);
-    }
-    Ok(())
+      // simplest solution to check if the value assignment returns an error:
+      // io::stdout().write_all(&buffer[..num_read])?;
+      // this syntax is intentionally not used in this case to allow the custom
+      // case to handle a broken pipe error type
+      let buffer = match read::read(&args.infile) {
+        Ok(x) if x.is_empty() => break,
+        Ok(x) => x,
+        Err(_) => break,
+      };
+      stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
+      if !write::write(&args.outfile, &buffer)? {
+        break;
+      }
+  }
+  stats::stats(args.silent, 0, &mut total_bytes, true);
+  Ok(())
 }
