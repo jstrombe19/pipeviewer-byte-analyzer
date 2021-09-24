626e5f02b6264b3e9ad19b5b1c9a9920cffca83a
Jared Stromberg <jaredstromberg@gmail.com>
2021-09-24 12:56:03 -0600

    modified read.rs module to support its own infinite loop within the read function.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use pipeviewer_byte_analyzer::{args::Args, read, stats, write};
 use std::io::Result;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() -> Result<()> {
   
@@ -9,6 +11,35 @@ fn main() -> Result<()> {
   // dbg!(silent);
 
   let args = Args::parse();
+  // destructuring of the struct - this is simpler than cloning the struct three times
+  let Args {
+    infile,
+    outfile,
+    silent,
+  } = args;
+
+  // create quit signals for each child thread and the parent thread. the children
+  // are cloned out of the parent
+  // the Arc will coordinate cleanup once the threads are no longer in use
+  // Mutex functions the same way it does in C
+  let quit = Arc::new(Mutex::new(false));
+  let (quit1, quit2, quit3) = (quit.clone(), quit.clone(), quit.clone());
+
+  let read_handle = thread::spawn(move || read::read_loop(&infile, quit1));
+  let stats_handle = thread::spawn(move || stats::stats_loop(silent, quit3));
+  let write_handle = thread::spawn(move || write::write_loop(&outfile, quit3));
+
+  // crash if any threads have crashed
+  // `.join()` returns a `thread::Result<io::Result<()>>`
+  let read_io_result = read_handle.join().unwrap();
+  let stats_io_result = stats_handle.join().unwrap();
+  let write_io_result = write_handle.join().unwrap();
+
+  // return an error if any threads returned an error
+  read_io_result?;
+  stats_io_result?;
+  write_io_result?;
+
   let mut total_bytes = 0;
   loop {
       // original position of buffer
--- src/read.rs
+++ src/read.rs
@@ -1,8 +1,9 @@
 use crate::CHUNK_SIZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
+use std::sync::{Arc, Mutex};
 
-pub fn read(infile: &str) -> Result<Vec<u8>> {
+pub fn read(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
   let mut reader: Box<dyn Read> = if !infile.is_empty() {
     Box::new(BufReader::new(File::open(infile)?))
   } else {
@@ -10,8 +11,18 @@ pub fn read(infile: &str) -> Result<Vec<u8>> {
   };
 
   let mut buffer = [0; CHUNK_SIZE];
+  loop {
+    let num_read = match reader.read(&mut buffer) {
+      Ok(0) => break,
+      Ok(x) => x,
+      Err(_) => break,
+    };
+    // todo: send this buffer to the stats thread
+    Vec::from(&buffer[..num_read]);
+  }
+  // todo: send an empty buffer to the stats thread
+  let mut quit = quit.lock().unwrap();
+  *quit = true;
 
-  let num_read = reader.read(&mut buffer)?;
-
-  Ok(Vec::from(&buffer[..num_read]))
+  Ok(())
 }
