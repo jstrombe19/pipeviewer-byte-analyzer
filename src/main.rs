5e70d00b57d98947fda8ffe95621060bc83854ce
Jared Stromberg <jaredstromberg@gmail.com>
2021-09-26 12:08:06 -0600

    updated thread management from Arc, Mutex to mpsc Sender, Receiver

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,51 @@
 use pipeviewer_byte_analyzer::{args::Args, read, stats, write};
 use std::io::Result;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 use std::thread;
 
 fn main() -> Result<()> {
-  
-  // to view the outcome or values of the variables, use the dbg! macro below
-  // dbg!(infile, outfile, silent);
-  // dbg! macro example - NOTE: this is NOT for logging!
-  // dbg!(silent);
+    // to view the outcome or values of the variables, use the dbg! macro below
+    // dbg!(infile, outfile, silent);
+    // dbg! macro example - NOTE: this is NOT for logging!
+    // dbg!(silent);
 
-  let args = Args::parse();
-  // destructuring of the struct - this is simpler than cloning the struct three times
-  let Args {
-    infile,
-    outfile,
-    silent,
-  } = args;
+    let args = Args::parse();
+    // destructuring of the struct - this is simpler than cloning the struct three times
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
+    // threads named after the thread receiving
+    // stats_tx is the transmitter sending data TO the stats thread
+    // stats_rx is the receiver insinde the stats thread
+    // mpsc :: multiple producer, single consumer
+    // means you can clone the _tx side of the channel and send it to multiple
+    // receivers, but you can only have a single receiver for a given channel
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
 
-  // create quit signals for each child thread and the parent thread. the children
-  // are cloned out of the parent
-  // the Arc will coordinate cleanup once the threads are no longer in use
-  // Mutex functions the same way it does in C
-  let quit = Arc::new(Mutex::new(false));
-  let (quit1, quit2, quit3) = (quit.clone(), quit.clone(), quit.clone());
+    // create quit signals for each child thread and the parent thread. the children
+    // are cloned out of the parent
+    // the Arc will coordinate cleanup once the threads are no longer in use
+    // Mutex functions the same way it does in C
+    // let quit = Arc::new(Mutex::new(false));
+    // let (quit1, quit2, quit3) = (quit.clone(), quit.clone(), quit.clone());
 
-  let read_handle = thread::spawn(move || read::read_loop(&infile, quit1));
-  let stats_handle = thread::spawn(move || stats::stats_loop(silent, quit2));
-  let write_handle = thread::spawn(move || write::write_loop(&outfile, quit3));
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, write_tx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
 
-  // crash if any threads have crashed
-  // `.join()` returns a `thread::Result<io::Result<()>>`
-  let read_io_result = read_handle.join().unwrap();
-  let stats_io_result = stats_handle.join().unwrap();
-  let write_io_result = write_handle.join().unwrap();
+    // crash if any threads have crashed
+    // `.join()` returns a `thread::Result<io::Result<()>>`
+    let read_io_result = read_handle.join().unwrap();
+    let stats_io_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
 
-  // return an error if any threads returned an error
-  read_io_result?;
-  stats_io_result?;
-  write_io_result?;
+    // return an error if any threads returned an error
+    read_io_result?;
+    stats_io_result?;
+    write_io_result?;
 
-  Ok(())
+    Ok(())
 }
--- src/write.rs
+++ src/write.rs
@@ -1,31 +1,27 @@
 use std::fs::File;
 use std::io::{self, BufWriter, ErrorKind, Result, Write};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::Receiver;
 
-pub fn write_loop(outfile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
-  let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-    Box::new(BufWriter::new(File::create(outfile)?))
-  } else {
-    Box::new(BufWriter::new(io::stdout()))
-  };
+pub fn write_loop(outfile: &str, write_rx: Receiver<Vec<u8>>) -> Result<()> {
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(BufWriter::new(io::stdout()))
+    };
 
-  loop {  
-  // todo: receive vector from stats thread
-  let buffer: Vec<u8> = Vec::new(); // so we can compile
-  {
-    let quit = quit.lock().unwrap();
-    if *quit {
-      break;
+    loop {
+        let buffer = write_rx.recv().unwrap();
+        if buffer.is_empty() {
+            break;
+        }
+        if let Err(e) = writer.write_all(&buffer) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                // stop the program cleanly
+                return Ok(());
+            }
+            return Err(e);
+        }
     }
-  }
-  if let Err(e) = writer.write_all(&buffer) {
-    if e.kind() == ErrorKind::BrokenPipe {
-      // stop the program cleanly
-      return Ok(());
-    }
-    return Err(e);
-    }
-  }
 
-  Ok(())
+    Ok(())
 }
